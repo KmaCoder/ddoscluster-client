6f1b9d134a55100a622e54d63a6a31cf0b1e21fd
Maksym Nesterov <maxim4110@gmail.com>
2022-03-01 21:32:26 +0200

    update readme

--- README.md
+++ README.md
@@ -18,8 +18,8 @@ python main.py [-h] --name NAME [--threads THREADS]
 
 options:
   -h, --help         show this help message and exit
-  --name NAME        Client name
-  --threads THREADS  Threads count
+  --name NAME        client name
+  --threads THREADS  connection threads count per process
 ```
 
 ### With docker
--- src/utils/args_utils.py
+++ src/utils/args_utils.py
@@ -5,7 +5,7 @@ from src.config import DEFAULT_THREADS_PER_PROCESS
 
 def get_cli_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--name', help='Client name', type=str, required=True)
-    parser.add_argument('--threads', help='Threads count', type=int, default=DEFAULT_THREADS_PER_PROCESS)
+    parser.add_argument('--name', help='client name', type=str, required=True)
+    parser.add_argument('--threads', help='connection threads count per process', type=int, default=DEFAULT_THREADS_PER_PROCESS)
 
     return parser.parse_args()
